ba90d7d4ccc0338edbcb5978e5a7b8c98f8856f2
advait0012 <advait0012@proton.me>
2024-09-29 02:15:30 +0530

    delete function in progress

--- main.js
+++ main.js
@@ -5,16 +5,26 @@ const btnEl = document.querySelector(".addTasks");
 const tasksList = document.querySelector(".lists");
 
 btnEl.addEventListener("click", addTasks);
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addTasks();
+  }
+});
 
 function addTasks() {
   if (input.value === "") {
-    alert('Invalid Task.');
+    alert("Invalid Task.");
   } else {
     const li = document.createElement("li");
     li.textContent += input.value;
     tasksList.appendChild(li);
-    li.style.padding = "1rem";
+    li.style.margin = "2rem 0 0 0";
     li.style.listStyleType = "none";
+    li.style.border = "1px solid #FF6500"
+    li.style.padding = "0.3rem 0 0.4rem 2rem";
+    li.style.boxShadow = "3px 3px 1px #FF6500"
+    li.style.borderRadius = "0.5rem"
+    li.style.width = "22rem"
     input.value = "";
   }
 }
