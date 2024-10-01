b49c70a7cd491b48ca90253b8103c5bf859087df
advait0012 <advait0012@proton.me>
2024-10-01 23:27:43 +0530

    local storage added

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-// const input = document.querySelector(".inputTask");
-// const btnEl = document.querySelector(".addTasks");
-// const tasksList = document.querySelector(".lists");
-
-// btnEl.addEventListener("click", addTasks);
-// input.addEventListener("keypress", (e) => {
-//   if (e.key === "Enter") {
-//     addTasks();
-//   }
-// });
-
-// function addTasks() {
-//   if (input.value === "") {
-//     alert("Invalid Task.");
-//   } else {
-//     const li = document.createElement("li");
-//     li.textContent += input.value;
-//     li.addEventListener("click", () => {
-//       if (li.style.textDecoration === "line-through") {
-//         li.style.textDecoration = "none";
-//       } else {
-//         li.style.textDecoration = "line-through";
-//       }
-//     });
-//     tasksList.appendChild(li);
-//     const span = document.createElement("span");
-//     span.textContent = "X";
-//     span.addEventListener("click", () => {
-//       tasksList.removeChild(li);
-//     });
-//     li.appendChild(span);
-//   }
-//   input.value = "";
-// }
-
 const input = document.querySelector(".inputTask");
 const btnEl = document.querySelector(".addTasks");
 const tasksList = document.querySelector(".lists");
